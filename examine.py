970481163bfc1aa023cf362c4529c93907c025e6
Sardorbek <ergashevsardorbek031@gmail.com>
2024-05-03 18:20:03 +0300

    examin

---
+++ examine.py
@@ -0,0 +1,56 @@
+#def number(lst):
+#    for x in range(lst):
+#        m = x == int
+#        return m
+#
+#
+#print(number(["9.4", "4.2"]))
+#print(number(["91", "44"]))
+#print(number(["9.5", "8.8"]))
+
+
+def rotate(lst):
+    for x in lst:
+        m = lst.pop(4)
+        return
+
+
+print(rotate([1,2,3,4,5]))
+print(rotate([6,5,8,9,7]))
+print(rotate([20,15,26,8,4]))
+
+
+
+def num(lst):
+    for x in lst:
+        for i in lst:
+            if i == lst[2]:
+                return i + x[0]
+
+print(num([
+    [1,2,3,4,5],
+    [5,6,7,8,9],
+    [20,21,34,56,100]
+]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
